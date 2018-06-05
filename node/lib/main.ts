cce16dbb9b15cabf26d06f664a358863a9295553
Mgen <m@mgenware.com>
2018-06-05 22:01:35 +0800

    Support parseJSON argument in node version

--- node/lib/main.ts
+++ node/lib/main.ts
@@ -9,29 +9,59 @@ const existsAsync = promisify(fs.exists);
 const mkdirAsync = promisify(fs.mkdir);
 
 const ROOT_DIR = 'huge_dir';
-const ITERATION = 10000;
 
 (async () => {
+  const args = process.argv.slice(2);
+
+  // iteration argument
+  if (args.length < 1) {
+    console.log('Missing iteration argument');
+    process.exit(1);
+  }
+
+  const iteration = parseInt(args[0], 10);
+  if (iteration <= 0) {
+    console.log(`Invalid iteration value: ${iteration}`);
+    process.exit(1);
+  }
+
+  // parseJSON argument
+  let parseJSON = false;
+  if (args.length >= 2 && args[1] === '--parse-json') {
+    parseJSON = true;
+  }
+
   if (await existsAsync(ROOT_DIR)) {
     await del([ROOT_DIR]);
   }
 
   await mkdirAsync(ROOT_DIR);
-  const paths: string[] = new Array(ITERATION);
+  const paths: string[] = new Array(iteration);
   const content = await readFileAsync('../common/bench_data.json');
-  for (let i = 0; i < ITERATION; i++) {
+  for (let i = 0; i < iteration; i++) {
     paths[i] = path.join(ROOT_DIR, i.toString() + '.json');
   }
 
   console.time('write');
-  console.log(`Creating ${ITERATION} files...`);
+  console.log(`Creating ${iteration} files...`);
   const writeJobs = paths.map((p) => writeFileAsync(p, content));
   await Promise.all(writeJobs);
   console.timeEnd('write');
 
-  console.log(`Reading ${ITERATION} files...`);
+  if (parseJSON) {
+    console.log(`Reading and parsing ${iteration} files...`);
+  } else {
+    console.log(`Reading ${iteration} files...`);
+  }
   console.time('read');
-  const readJobs = paths.map((p) => readFileAsync(p));
+  const readJobs = paths.map(async (p) => {
+    const bytes = await readFileAsync(p);
+    if (parseJSON) {
+      return JSON.parse(bytes.toString());
+    } else {
+      return bytes;
+    }
+  });
   await Promise.all(readJobs);
   console.timeEnd('read');
 
